1de38717bcedbff890831c50baf263e3754b5989
Edosicca <47154408+sichej@users.noreply.github.com>
2022-02-18 09:33:58 +0100

    .

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+var l = [
+    {
+        "product":"caffe",
+        "price": "1,20",
+        "ingridients": ""
+    },
+    {
+        "product":"cappuccino",
+        "price": "1,50",
+        "ingridients": ""
+    },
+    {
+        "product":"ginseng",
+        "price": "1,80",
+        "ingridients": ""
+    },
+    {
+        "product":"sicca",
+        "price": "1,80",
+        "ingridients": "insalata | salame | maionese"
+    }
+]
+
+var i = 0;
+for( i = 0; i < l.length; i++){
+    console.log(l[i]);
+    var str = "<div class='menu-item'>ciao</div>";
+    //document.getElementById("menu").innerHTML += str;
+}
+
+//console.log(l[0].price)
