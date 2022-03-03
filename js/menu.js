96bf18ec600f25dda40ca5b4c55db64e0a73a3d3
Edosicca <47154408+sichej@users.noreply.github.com>
2022-03-03 12:19:06 +0100

    .

--- js/menu.js
+++ js/menu.js
@@ -445,9 +445,9 @@ var dishes = [
         "ingridients": "fesa di tacchino | patate al forno | rucola | glassa di aceto balsamico"
     },
     {
-        "product":"Contorno a scelta",
+        "product":"",
         "price": "",
-        "ingridients": "patate al forno | verdure grigliate"
+        "ingridients": "Contorno a scelta: patate al forno | verdure grigliate"
     }
 ]
 
